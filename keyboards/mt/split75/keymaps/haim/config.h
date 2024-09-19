95fafda4f0fd5e8ec5bba66073fb68485f0ec6ce
Haim Gelfenbeyn <haim@g8n.me>
2024-09-19 16:44:21 -0400

    Try to make the keyboard work consistently with Macs and my USB switch

--- keyboards/mt/split75/keymaps/haim/config.h
+++ keyboards/mt/split75/keymaps/haim/config.h
@@ -3,3 +3,7 @@
 
 // Do not do effects by default
 #undef RGBLIGHT_DEFAULT_MODE
+
+// This prevents hangups when M1/M2 MacBook is reconnected. This disables the USB suspend feature
+// altogether, though... Nothing else appears to work.
+#define NO_USB_STARTUP_CHECK
